b66f77d717baf185bcfbddf470edcf4ddb77ded5
Jacob Roberts <jacobo0421@gmail.com>
2022-11-14 00:11:09 -0500

    q

--- YoutubeReplica.js
+++ YoutubeReplica.js
@@ -1,7 +1,23 @@
-function Animationtoggle(x) {
+
+
+$(document).ready(function() {
+    $(".settings-container").click(function() {
+        $(".settings-container").addClass("transform");
+        $(".settings-container").on("webkitAnimationEnd oanimationend msAnimationEnd animationend", function(){
+            $(this).removeClass("transform");
+        });
+    });
+
+    
+    $(".voice-button").click(function() {
+        $(".voice-button").addClass("transform");
+        $(".voice-button").on("webkitAnimationEnd oanimationend msAnimationEnd animationend", function(){
+            $(this).removeClass("transform");
+        });
+    });
+
+});
+
+function AnimationToggle(x) {
     x.classList.toggle("transform");
 };
-
-$('.settings-container').onClick(function(){
-    $(this).addClass('transform');
-});
